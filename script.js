304fbabc7e39e39a713ce2afd57b0ed9bb97ba4d
anshu9015 <anshu.kmr9015@gmail.com>
2023-10-31 14:41:12 +0530

    notes-app created

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
 createBtn.addEventListener("click",()=>{
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
@@ -13,5 +22,21 @@ createBtn.addEventListener("click",()=>{
 notesContainer.addEventListener("click", function(e){
     if(e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+        updateStorage();
+    }
+    else if(e.target.tagName === "p"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
